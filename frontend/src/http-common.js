c7479cffc2e5ab2206ef97fcabaeae0ce1b0662d
Voltraxx <juan.cardenas.r@usach.cl>
2024-11-30 18:59:21 -0300

    .

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const prestabancoServer = import.meta.env.VITE_PRESTABANCO_SERVER;
 const prestabancoPort = import.meta.env.VITE_PRESTABANCO_PORT;
-
+console.log("Base URL:", `http://${prestabancoServer}:${prestabancoPort}`);
 export default axios.create({
     baseURL: `http://${prestabancoServer}:${prestabancoPort}`,
     headers: {
--- microservicio2/ms2/src/main/java/ms2/main/Controllers/UserController.java
+++ microservicio2/ms2/src/main/java/ms2/main/Controllers/UserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
-@CrossOrigin("*")
 public class UserController {
     @Autowired
     UserService userService;
