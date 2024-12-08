cc2b3b71c51e04140b613f8918d23620a7f3bb26
Voltraxx <juan.cardenas.r@usach.cl>
2024-12-08 19:47:48 -0300

    xd

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const prestabancoServer = import.meta.env.VITE_PRESTABANCO_SERVER;
 const prestabancoPort = import.meta.env.VITE_PRESTABANCO_PORT;
-console.log("Base URL:", `http://172.27.38.69:32751`);
+console.log("Base URL:", `http://172.27.38.69:31553`);
 export default axios.create({
-    baseURL: `http://172.27.38.69:32751`,
+    baseURL: `http://172.27.38.69:31553`,
     headers: {
         'Content-Type': 'application/json'
     },
