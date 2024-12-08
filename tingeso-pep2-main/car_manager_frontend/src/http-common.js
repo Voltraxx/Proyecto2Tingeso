85beac026efc5d563b5ebc4e3814d17823d53f9b
Voltraxx <juan.cardenas.r@usach.cl>
2024-12-08 03:41:54 -0300

    Fix: Remove secret from application.properties

--- tingeso-pep2-main/car_manager_frontend/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    //xsdsdsdsdsds
-    baseURL: `http://localhost:8080`,
-    headers: {
-        'Content-Type': 'application/json'
-    }
-});
